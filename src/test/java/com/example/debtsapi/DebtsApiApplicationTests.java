21f2674ea2db79922c4ff5df2949683d3a911eb0
mamoreno <mmorenoragni@gmail.com>
2023-04-02 19:33:30 -0300

    se sube estructura del proyecto

---
+++ src/test/java/com/example/debtsapi/DebtsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.debtsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
