1309032b534b21835cc07442e813d35f26dba77d
Mariano Eduardo Moreno Ragni <mamoreno@ar0c02vv0p1hv2j.fibertel.com.ar>
2023-04-12 16:30:22 -0300

    Proyecto solo Cuenta

--- src/main/java/com/example/debtsapi/controllers/HomeController.java
+++ src/main/java/com/example/debtsapi/controllers/HomeController.java
@@ -1,9 +1,7 @@
 package com.example.debtsapi.controllers;
 
 import com.example.debtsapi.entities.Deuda;
-import com.example.debtsapi.entities.Usuario;
 import com.example.debtsapi.service.DeudaService;
-import com.example.debtsapi.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,34 +14,7 @@ import java.util.Optional;
 public class HomeController {
 
     @Autowired
-    UsuarioService usuarioService;
-
-    @Autowired
     DeudaService deudaService;
-
-    @PostMapping("/crearUsuario")
-    public void crearUsuario(@RequestBody Usuario usuario){
-      usuarioService.crearUsuario(usuario.getNombre(), usuario.getApellido(), usuario.getEmail());
-    }
-
-    @PostMapping("/buscarUsuario")
-    public Optional<Usuario> buscarUsuario(@RequestBody Usuario usuario){
-
-        Optional<Usuario> userId= usuarioService.buscarUsuario(usuario.getIdUsuario());
-
-        return userId;
-    }
-
-    @PostMapping("/modificarUsuario")
-    public void actualizar(@RequestBody Usuario usuario){
-        usuarioService.modificarUsuario(usuario.getNombre(), usuario.getApellido(), usuario.getEmail(), usuario.getIdUsuario());
-    }
-
-    @PostMapping("/borrarUsuario")
-    public String borrar(@RequestBody Usuario usuario){
-    usuarioService.borrarUsuario(usuario.getIdUsuario());
-    return "Registro eliminado correctamente";
-    }
 
     //---------------------------------------------------------------------------------------------------------
     //                                          Crud Deuda
